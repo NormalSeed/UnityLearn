35b8ea141790cf95227175628d032725827e4f6c
NormalSeed <dlrjsghk11@naver.com>
2025-04-22 16:43:39 +0900

    [Feat] Make player move
    PlayerController 스크립트에 플레이어 이동 구현

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    [SerializeField] float speed;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(movementX, 0, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnMove (InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 }
