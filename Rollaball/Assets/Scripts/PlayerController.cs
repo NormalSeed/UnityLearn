4d3d7d911b11bdde598961495ca61e55207e8bd3
NormalSeed <dlrjsghk11@naver.com>
2025-04-23 00:05:17 +0900

    [Feat] Add Enemies, PickUps
    적과 PickUp의 갯수를 늘림

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -54,7 +54,7 @@ public class PlayerController : MonoBehaviour
     private void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 4)
+        if (count >= 10)
         {
             winTextObject.SetActive(true);
             Destroy(GameObject.FindGameObjectWithTag("Enemy"));
